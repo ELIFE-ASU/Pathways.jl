848386c286502cabbe70a9a43ba153ba4927d3cf
Douglas G. Moore <doug@dglmoore.com>
2020-07-30 14:44:42 -0700

    Use vectors instead of sets

--- src/assembly.jl
+++ src/assembly.jl
@@ -1,18 +1,22 @@
 using Base.Iterators
 
 const Split = Vector
-const SplitTree = Dict{T, Set{Split{T}}} where T
-const Cache = Dict{NTuple{2, Set{T}}, Int} where T
+const SplitTree = Dict{T, Vector{Split{T}}} where T
+const Cache = Dict{NTuple{2, Vector{T}}, Int} where T
 
 function splittree(s::S) where {S <: AbstractString}
     st = SplitTree{S}()
     stack = [s]
     while !isempty(stack)
         str = pop!(stack)
-        st[str] = Set{Split{S}}()
+        st[str] = Vector{Split{S}}()
         for i in 1:length(str)-1
             a, b = str[1:i], str[i+1:end]
             a, b = a <= b ? (a, b) : (b, a)
+            xs = [a, b]
+            if xs in st[str]
+                continue
+            end
             push!(st[str], [a, b])
             if !haskey(st, a)
                 push!(stack, a)
@@ -25,24 +29,23 @@ function splittree(s::S) where {S <: AbstractString}
     st
 end
 
-function assembly(st::SplitTree{S}, ss::Set{S}, sc::Set{S}, cache::Cache{S}) where {S <: AbstractString}
+function assembly(st::SplitTree{S}, ss::Vector{S}, sc::Vector{S}, cache::Cache{S}) where {S <: AbstractString}
     if haskey(cache, (ss, sc))
         cache[(ss, sc)]
     else
         cc = typemax(Int)
         components = (st[s] for s in ss)
         for group in product(components...)
-            uv = vcat(group...)
-            filter!(!isbasic, uv)
-            objects = Set(uv)
-            scs = shortcuts(objects)
-            union!(scs, sc)
+            objects = vcat(group...)
+            filter!(!isbasic, objects)
             setdiff!(objects, sc)
             if isempty(objects)
-            	cc = 0
-            	break
+                cc = 0
+                break
             else
-            	cc = min(cc, assembly(st, objects, scs, cache))
+                scs = shortcuts(objects)
+                union!(scs, sc)
+                cc = min(cc, assembly(st, objects, scs, cache))
             end
         end
         cc += length(ss)
@@ -52,13 +55,13 @@ end
 
 function assembly(s::S...; cache::Cache{S}=Cache{S}()) where {S <: AbstractString}
     st = merge(SplitTree{S}(), splittree.(s)...)
-    ss = Set(s)
+    ss = collect(s)
     sc = shortcuts(ss)
     setdiff!(ss, sc)
     assembly(st, ss, sc, cache)
 end
 
-shortcuts(ss::Set{<:AbstractString}) = filter(s -> any(t -> isbelow(s, t), ss), ss)
+shortcuts(ss) = filter(s -> any(t -> isbelow(s, t), ss), ss)
 
 isbelow(s::S, t::S) where {S <: AbstractString} = length(s) != length(t) && !isnothing(findfirst(s, t))
 
